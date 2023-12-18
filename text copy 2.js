df55ef793a58a6e749a9a9ecc4a59b398729de17
Mintzs <yoelnathanael1@gmail.com>
2023-12-18 13:32:53 +0700

    a

--- text copy 2.js
+++ text copy 2.js
@@ -81,6 +81,56 @@ const pointfunc = () => {
     scene.add(mesh);
 };
 
+const addPeashooter = () => {
+  
+    const headGeometry = new THREE.SphereGeometry(2.5, 64, 64);
+    const headMaterial = new THREE.MeshPhongMaterial({ color: '#52D017', shininess: 30 });
+    const head = new THREE.Mesh(headGeometry, headMaterial);
+    head.position.set(-30, 10, 0);
+    head.castShadow = true;
+    scene.add(head);
+
+
+    const mouthGeometry = new THREE.CylinderGeometry(0.5, 1, 2.5, 64, 64, true);
+    const mouthMaterial = new THREE.MeshPhongMaterial({ color: '#52D017', shininess: 30 });
+    const mouth = new THREE.Mesh(mouthGeometry, mouthMaterial);
+    mouth.position.set(-26.5, 10, 0);
+    mouth.rotation.z = Math.PI / 2; 
+    mouth.castShadow = true;
+    scene.add(mouth);
+
+
+    const headTopGeometry = new THREE.ConeGeometry(1, 2.5, 64);
+    const headTopMaterial = new THREE.MeshPhongMaterial({ color: '#43B000', shininess: 30 });
+    const headTop = new THREE.Mesh(headTopGeometry, headTopMaterial);
+    headTop.position.set(-32.5, 12, 0);
+    headTop.rotation.z = Math.PI / 4;
+    headTop.castShadow = true;
+    scene.add(headTop);
+
+ 
+    const eyeGeometry = new THREE.SphereGeometry(0.5, 64, 64);
+    const eyeMaterial = new THREE.MeshPhongMaterial({ color: '#000000' });
+
+    const eye1 = new THREE.Mesh(eyeGeometry, eyeMaterial);
+    eye1.position.set(-28.5, 11, -1.5);
+    eye1.castShadow = true;
+    scene.add(eye1);
+
+    const eye2 = new THREE.Mesh(eyeGeometry, eyeMaterial);
+    eye2.position.set(-28.5, 11, 1.5);
+    eye2.castShadow = true;
+    scene.add(eye2);
+
+
+    const trunkGeometry = new THREE.CylinderGeometry(0.75, 0.75, 10, 64, 64, true);
+    const trunkMaterial = new THREE.MeshPhongMaterial({ color: '#4BBF15', shininess: 30 });
+    const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    trunk.position.set(-30, 5, 0);
+    trunk.castShadow = true;
+    scene.add(trunk);
+};
+
 const loaderZombie = () => {
     loader.load("./Assets/zombie/scene.gltf", function (GLTF) {
         let zombie = GLTF.scene;
@@ -112,6 +162,63 @@ const loaderFence = () => {
         fence.castShadow = true;
         scene.add(fence);
     });
+
+    loader.load("./Assets/fence/scene.gltf", function (GLTF) {
+        let fence1 = GLTF.scene;
+        fence1.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        fence1.scale.set(10, 10, 10);
+        fence1.position.set(-20, 8.5, -44);
+        fence1.castShadow = true;
+        scene.add(fence1);
+    });
+
+    loader.load("./Assets/fence/scene.gltf", function (GLTF) {
+        let fence2 = GLTF.scene;
+        fence2.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        fence2.scale.set(10, 10, 10);
+        fence2.position.set(0, 8.5, -44);
+        fence2.castShadow = true;
+        scene.add(fence2);
+    });
+
+    loader.load("./Assets/fence/scene.gltf", function (GLTF) {
+        let fence3 = GLTF.scene;
+        fence3.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        fence3.scale.set(10, 10, 10);
+        fence3.position.set(20, 8.5, -44);
+        fence3.castShadow = true;
+        scene.add(fence3);
+    });
+
+    loader.load("./Assets/fence/scene.gltf", function (GLTF) {
+        let fence4 = GLTF.scene;
+        fence4.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        fence4.scale.set(10, 10, 10);
+        fence4.position.set(40, 8.5, -44);
+        fence4.castShadow = true;
+        scene.add(fence4);
+    });
+
 };
 
 const addPlantsNoZombie = () => {
@@ -134,6 +241,7 @@ const addPlantsNoZombie = () => {
         }
     );
 };
+
 
 const initSky = () => {
     day = new THREE.CubeTextureLoader()
@@ -159,8 +267,23 @@ const initSky = () => {
     scene.background = day;
 };
 
+const walnut =() =>{
+    const walnutGeometry = new THREE.CylinderGeometry(4.5, 4.5, 3, 64, 64, true);
+    const texture = loader.load("./img/green-grass-field-background-soccer-football-sports-lawn-pattern-texture-close-up-image-142564163.jpg");
+    
+    const walnutMaterial = new THREE.MeshPhongMaterial({ color: '#4BBF15', map: texture });
+    
+    const walnut = new THREE.Mesh(walnutGeometry, walnutMaterial);
+    walnut.position.set(-17.5, 4.5, 0);
+    walnut.rotation.z = Math.PI / 2;
+    walnut.castShadow = true;
+    
+    scene.add(walnut);
+ 
+}
+
 light = () => {
-    // Declare the ambient light globally
+
     light = new THREE.AmbientLight('#FFFFFC');
     light.position.set(0, 0, 0);
     light.intensity = 0.5;
@@ -170,7 +293,7 @@ light = () => {
 };
 
 spotlight = () => {
-    // Declare the spotlight globally
+
     spotlight = new THREE.SpotLight('#FFFFFF');
     spotlight.position.set(-80, 40, 0);
     spotlight.intensity = 1.2;
@@ -194,10 +317,14 @@ window.onload = () => {
     loaderZombie();
     loaderFence();
     addPlantsNoZombie();
+    walnut();
+    addPeashooter();
     initSky();
     light();
     spotlight();
     render();
+
+
 };
 
 window.onresize = () => {
